170deb8658696d408b32c98fe679b49f9248e46e
Rafael Ferreira <faelxbox83@hotmail.com>
2025-03-14 16:27:35 -0300

    Tipos e variaveis - String, int, short e VALOR DE PI

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        
+        int numero = 5;
+
+        numero = 2;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
